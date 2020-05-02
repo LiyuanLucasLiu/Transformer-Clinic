087f7b514f86c6f854a8019caf509e0883c7189b
Liyuan Liu <llychinalz@gmail.com>
2020-05-02 14:00:46 -0500

    Update eval_wmt_en-de.sh

--- nmt-experiments/eval_wmt_en-de.sh
+++ nmt-experiments/eval_wmt_en-de.sh
@@ -16,5 +16,5 @@ fi
 CUDA_VISIBLE_DEVICES=$GPUDEV fairseq-generate data-bin/wmt14_en_de_joined_dict \
                     --path $MODELDIR \
                     --batch-size 128 --beam 5 --remove-bpe \
-                    --user-dir ./radam_fairseq --quiet
+                    --user-dir ../radam_fairseq --quiet
 
